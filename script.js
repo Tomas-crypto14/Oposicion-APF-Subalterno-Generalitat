99a8a72bc50a9eadb3c4e5bb60178d7111b47515
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2026-02-18 18:26:46 +0100

    Temario actualizado con artículos añadidos

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-echo("Hola");
+console.log("Hola");
+
+const seleccion = document.getElementById("selector-equipos");
+const seleccion2 = document.getElementById("selector-equipos2");
+
+function tema(event){
+    const selectTema=event.target.value;
+    if(selectTema){
+        window.location.href = selectTema
+    }
+}
+
+seleccion.addEventListener("change", tema);
+seleccion2.addEventListener("change", tema);
